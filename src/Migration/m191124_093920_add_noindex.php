6ad7bcbd8bdb817d4e214d4ef3a635c7d67843a3
stupidkitty <adx.informer@protonmail.com>
2019-11-24 12:59:28 +0300

    Add noindex,nofollow to videos, tested_at to videos_stats

---
+++ src/Migration/m191124_093920_add_noindex.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191124_093920_add_noindex
+ */
+class m191124_093920_add_noindex extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute('ALTER TABLE `videos` ADD `noindex` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `on_index`,
+                        ADD `nofollow` TINYINT UNSIGNED NOT NULL DEFAULT 0
+                        AFTER `noindex`');
+
+        $this->execute('ALTER TABLE `videos_stats` ADD `tested_at` TIMESTAMP NULL DEFAULT NULL AFTER `tested_image`');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('videos', 'noindex');
+        $this->dropColumn('videos', 'nofollow');
+        $this->dropColumn('videos_stats', 'tested_at');
+    }
+}
