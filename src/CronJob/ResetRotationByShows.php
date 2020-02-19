06d7f00e913db44bbf89b68714190c94b0182133
stupidkitty <adx.informer@protonmail.com>
2020-02-19 17:00:33 +0300

    Add cyclic reset rotation job

---
+++ src/CronJob/ResetRotationByShows.php
@@ -0,0 +1,19 @@
+<?php
+namespace SK\VideoModule\CronJob;
+
+use SK\VideoModule\Service\Rotator;
+use SK\CronModule\Handler\HandlerInterface;
+
+/**
+ * Сброс отротированных видео по показам.
+ *
+ * @return void
+ */
+class ResetRotationByShows implements HandlerInterface
+{
+    public function run()
+    {
+        $rotator = new Rotator;
+        $rotator->cyclicResetByShows();
+    }
+}
