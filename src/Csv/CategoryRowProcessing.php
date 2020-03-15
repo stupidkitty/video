6a959123b63c45035f9ef91de2b03c14090d9f71
Alikdex <alikdex@gmail.com>
2020-03-15 14:36:58 +0300

    Rework row processor

--- src/Csv/CategoryRowProcessing.php
+++ src/Csv/CategoryRowProcessing.php
@@ -3,48 +3,114 @@ namespace SK\VideoModule\Csv;
 
 use Csv\Item;
 use Csv\RowHandlerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use SK\VideoModule\Csv\CategoryImportOptions;
+use SK\VideoModule\Event\CsvFailedItemEvent;
+use SK\VideoModule\Model\Category;
 
 class CategoryRowProcessing implements RowHandlerInterface
 {
     private $options;
-    private $fields;
+    private $eventDispatcher;
 
-    public function __construct(array $fields, CategoryImportOptions $options)
+    /**
+     * CategoryRowProcessing constructor
+     *
+     * @param CategoryImportOptions $options
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(CategoryImportOptions $options, EventDispatcherInterface $eventDispatcher)
     {
-        $this->fields = $fields;
         $this->options = $options;
-      }
+        $this->eventDispatcher = $eventDispatcher;
+    }
 
-    public function success(Item $row): void
+    /**
+     * Accepted items handle
+     *
+     * @param Item $item
+     * @return void
+     */
+    public function success(Item $item): void
     {
+        $errors = [];
+
         try {
-            //echo 'Success:' . $row->get(0);
-            //$data = $this->prepareRow($row);
-            dump($row);
+            // Если название пустое, значит оно будет идом категории.
+            if (!$item->has('title')) {
+                $item->put('title', $item->get('category_id'));
+            }
+
+            $category = Category::find()
+                ->orFilterWhere(['category_id' => $item->get('category_id')])
+                ->orFilterWhere(['title' => $item->get('title')])
+                ->orFilterWhere(['slug' => $item->get('slug')])
+                ->one();
+
+            // Если нашлась категория, но обновлять не нужно - выходим.
+            if (null !== $category && false === $this->options->isUpdate) {
+                throw new \Exception("Category exists: {$category->title}");
+            } elseif (null === $category) {
+                $category = new Category;
+            }
+
+            if ($item->has('category_id')) {
+                $category->category_id = $item->get('category_id');
+            }
+
+            if ($item->has('meta_description')) {
+                $item->put('meta_description', \mb_substr($item->get('meta_description'), 0, 255));
+            }
+
+            if ($item->has('meta_title')) {
+                $item->put('meta_title', \mb_substr($item->get('meta_title'), 0, 255));
+            }
+
+            $category->setAttributes($item->toArray());
+
+            if (!$category->slug) {
+                $slug = $item->get('title');
+                $category->generateSlug($slug);
+            } elseif ($category->slug && true === $this->options->isReplaceSlug) {
+                $slug = $item->get('slug', $item->get('title'));
+                $category->generateSlug($slug);
+            }
+
+            if ($this->options->isEnable) {
+                $category->enable();
+            }
+
+            if (!$category->isNewRecord) {
+                $category->updated_at = \gmdate('Y-m-d H:i:s');
+            }
+
+            if (!$category->save()) {
+                $errors = $category->getErrorSummary(true);
+
+                throw new \Exception("Category validation fail: {$category->title}");
+            }
         } catch (\Throwable $e) {
-            \Yii::warning($e->getMessage());
+            $this->eventDispatcher->dispatch(new CsvFailedItemEvent($item, $e->getMessage(), $errors), CsvFailedItemEvent::NAME_CATEGORY_IMPORT);
         }
     }
 
-    public function failure(Item $row): void
+    /**
+     * Invalid items handle
+     *
+     * @param Item $item
+     * @return void
+     */
+    public function failure(Item $item): void
     {
-        echo 'fail:';
-        dump($row);
+        $this->eventDispatcher->dispatch(new CsvFailedItemEvent($item), CsvFailedItemEvent::NAME_CATEGORY_IMPORT);
     }
 
+    /**
+     * Exec after hanle all items
+     *
+     * @return void
+     */
     public function eof(): void
     {
     }
-
-    /*private function prepareRow($row)
-    {
-        $data = [];
-        $arrayValues = array_values($row['items']);
-        foreach ($this->fields as $key => $field) {
-            $data[$field] = $row->get($key);
-        }
-        dump($row, $data, $this->fields, $arrayValues); exit;
-        return $data;
-    }*/
 }
